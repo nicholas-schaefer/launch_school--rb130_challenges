ac597aed160e76962d9548ead60ccd88bb1a9614
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-04-17 14:25:15 -0400

    all easy_02 tests passed!

--- easy/02/lib/point_mutation_challenge.rb
+++ easy/02/lib/point_mutation_challenge.rb
@@ -1,9 +1,23 @@
 require 'bundler/setup'
 
 class DNA
-  def initialize(sequence)
+  def initialize(original_sequence)
+    self.original_sequence = original_sequence
   end
 
-  def hamming_distance(arg1)
+  def hamming_distance(second_sequence)
+    self.second_sequence = second_sequence
+    self.smallest_char_count = count_chars_to_compare
+    (0...smallest_char_count).reject {|x| original_sequence[x] == second_sequence[x] }.count
   end
+
+  private
+  attr_accessor :original_sequence, :second_sequence, :smallest_char_count
+
+  def count_chars_to_compare
+    original_length = original_sequence.length
+    second_length = second_sequence.length
+    (original_length < second_length) ? original_length : second_length
+  end
+
 end
