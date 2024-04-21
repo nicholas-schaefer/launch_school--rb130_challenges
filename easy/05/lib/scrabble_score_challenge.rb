1c1dcf8382ce7a674bf798b1864cb7e74a5d2148
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-04-21 16:29:55 -0400

    easy_05 all tests complete!

--- easy/05/lib/scrabble_score_challenge.rb
+++ easy/05/lib/scrabble_score_challenge.rb
@@ -1,1 +1,30 @@
 require "bundler/setup"
+
+class Scrabble
+
+  def self.score(word_str)
+    self.new(word_str).score
+  end
+
+  CONSTANT = {
+    "a" => 1, "b" => 3, "c" => 3, "d" => 2,
+    "e" => 1, "f" => 4, "g" => 2, "h" => 4,
+    "i" => 1, "j" => 8, "k" => 5, "l" => 1,
+    "m" => 3, "n" => 1, "o" => 1, "p" => 3,
+    "q" => 10, "r" => 1, "s" => 1, "t" => 1,
+    "u" => 1, "v" => 4, "w" => 4, "x" => 8,
+    "y" => 4, "z" => 10,
+  }
+
+  attr_reader :word_str
+  def initialize(word_str)
+    @word_str = word_str
+  end
+
+  def score
+    return 0 if word_str.nil? || word_str.strip.empty?
+    word_str.downcase.each_char.reduce(0) do |sum, char|
+      sum + CONSTANT[char]
+    end
+  end
+end
--- easy/05/test/scrabble_score_challenge_test.rb
+++ easy/05/test/scrabble_score_challenge_test.rb
@@ -5,48 +5,62 @@ require_relative '../lib/scrabble_score_challenge.rb'
 
 Minitest::Reporters.use!
 
+=begin
+You'll need the following tile scores:
+Letter 	Value
+A, E, I, O, U, L, N, R, S, T 	1
+D, G 	2
+B, C, M, P 	3
+F, H, V, W, Y 	4
+K 	5
+J, X 	8
+Q, Z 	10
+
+How to Score
+=end
+
 class ScrabbleTest < Minitest::Test
   def test_empty_word_scores_zero
     assert_equal 0, Scrabble.new('').score
   end
 
   def test_whitespace_scores_zero
-    skip
+    #skip
     assert_equal 0, Scrabble.new(" \t\n").score
   end
 
   def test_nil_scores_zero
-    skip
+    #skip
     assert_equal 0, Scrabble.new(nil).score
   end
 
   def test_scores_very_short_word
-    skip
+    #skip
     assert_equal 1, Scrabble.new('a').score
   end
 
   def test_scores_other_very_short_word
-    skip
+    #skip
     assert_equal 4, Scrabble.new('f').score
   end
 
   def test_simple_word_scores_the_number_of_letters
-    skip
+    #skip
     assert_equal 6, Scrabble.new('street').score
   end
 
   def test_complicated_word_scores_more
-    skip
+    #skip
     assert_equal 22, Scrabble.new('quirky').score
   end
 
   def test_scores_are_case_insensitive
-    skip
+    #skip
     assert_equal 41, Scrabble.new('OXYPHENBUTAZONE').score
   end
 
   def test_convenient_scoring
-    skip
+    #skip
     assert_equal 13, Scrabble.score('alacrity')
   end
 end
