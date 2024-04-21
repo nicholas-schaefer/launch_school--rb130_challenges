1e8ca4cd06315954894a45d562b90a46a59a6a83
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-04-21 11:28:46 -0400

    easy_10 all tests passed!

--- easy/10/lib/series_challenge.rb
+++ easy/10/lib/series_challenge.rb
@@ -1,3 +1,14 @@
 require 'bundler/setup'
 
-puts "here we are!"
+class Series
+  attr_reader :string_of_digits
+  def initialize(string_of_digits)
+    @string_of_digits = string_of_digits
+  end
+
+  def slices(num_of_slices)
+    raise ArgumentError unless (1..string_of_digits.length).any?(num_of_slices)
+    array_of_digits = string_of_digits.chars.map(&:to_i)
+    array_of_digits.each_cons(num_of_slices).to_a
+  end
+end
--- easy/10/test/series_challenge_test.rb
+++ easy/10/test/series_challenge_test.rb
@@ -5,6 +5,26 @@ require_relative '../lib/series_challenge.rb'
 
 Minitest::Reporters.use!
 
+=begin
+Write a program that will take a string of digits
+and return all the possible consecutive number series
+of a specified length in that string.
+
+For example, the string "01234" has the following 3-digit series:
+
+    012
+    123
+    234
+
+Likewise, here are the 4-digit series:
+
+    0123
+    1234
+
+Finally, if you ask for a 6-digit series from a 5-digit string,
+you should throw an error.
+=end
+
 class SeriesTest < Minitest::Test
   def test_simple_slices_of_one
     series = Series.new('01234')
@@ -12,75 +32,75 @@ class SeriesTest < Minitest::Test
   end
 
   def test_simple_slices_of_one_again
-    skip
+    #skip
     series = Series.new('92834')
     assert_equal [[9], [2], [8], [3], [4]], series.slices(1)
   end
 
   def test_simple_slices_of_two
-    skip
+    #skip
     series = Series.new('01234')
     assert_equal [[0, 1], [1, 2], [2, 3], [3, 4]], series.slices(2)
   end
 
   def test_other_slices_of_two
-    skip
+    #skip
     series = Series.new('98273463')
     expected = [[9, 8], [8, 2], [2, 7], [7, 3], [3, 4], [4, 6], [6, 3]]
     assert_equal expected, series.slices(2)
   end
 
   def test_simple_slices_of_two_again
-    skip
+    #skip
     series = Series.new('37103')
     assert_equal [[3, 7], [7, 1], [1, 0], [0, 3]], series.slices(2)
   end
 
   def test_simple_slices_of_three
-    skip
+    #skip
     series = Series.new('01234')
     assert_equal [[0, 1, 2], [1, 2, 3], [2, 3, 4]], series.slices(3)
   end
 
   def test_simple_slices_of_three_again
-    skip
+    #skip
     series = Series.new('31001')
     assert_equal [[3, 1, 0], [1, 0, 0], [0, 0, 1]], series.slices(3)
   end
 
   def test_other_slices_of_three
-    skip
+    #skip
     series = Series.new('982347')
     expected = [[9, 8, 2], [8, 2, 3], [2, 3, 4], [3, 4, 7]]
     assert_equal expected, series.slices(3)
   end
 
   def test_simple_slices_of_four
-    skip
+    #skip
     series = Series.new('01234')
     assert_equal [[0, 1, 2, 3], [1, 2, 3, 4]], series.slices(4)
   end
 
   def test_simple_slices_of_four_again
-    skip
+    #skip
     series = Series.new('91274')
     assert_equal [[9, 1, 2, 7], [1, 2, 7, 4]], series.slices(4)
   end
 
   def test_simple_slices_of_five
-    skip
+    #skip
     series = Series.new('01234')
     assert_equal [[0, 1, 2, 3, 4]], series.slices(5)
   end
 
   def test_simple_slices_of_five_again
-    skip
+    #skip
     series = Series.new('81228')
     assert_equal [[8, 1, 2, 2, 8]], series.slices(5)
   end
 
   def test_simple_slice_that_blows_up
-    skip
+    #skip
     series = Series.new('01234')
     assert_raises ArgumentError do
       series.slices(6)
@@ -88,7 +108,7 @@ class SeriesTest < Minitest::Test
   end
 
   def test_more_complicated_slice_that_blows_up
-    skip
+    #skip
     slice_string = '01032987583'
 
     series = Series.new(slice_string)
