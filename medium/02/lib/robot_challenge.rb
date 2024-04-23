ad1cf8913130877813489b713333d68c6f0ce7de
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-04-23 16:45:23 -0400

    medium_02 all tests passed!

--- medium/02/lib/robot_challenge.rb
+++ medium/02/lib/robot_challenge.rb
@@ -1,1 +1,34 @@
 require 'bundler/setup'
+
+class Robot
+
+  @@names = []
+
+  UPPERCASE_LETTERS = ("A".."Z").to_a
+  DIGIT_STRINGS = ("0".."9").to_a
+
+  attr_reader :name
+  def initialize
+    reset()
+  end
+
+  def reset
+    self.name =
+      loop do
+        potential_name = random_untaken_name()
+        break potential_name unless @@names.include?(potential_name)
+      end
+      @@names << self.name
+    nil
+  end
+
+  private
+  attr_writer :name
+  def random_untaken_name
+    UPPERCASE_LETTERS.sample +
+    UPPERCASE_LETTERS.sample +
+    DIGIT_STRINGS.sample +
+    DIGIT_STRINGS.sample +
+    DIGIT_STRINGS.sample
+  end
+end
--- medium/02/test/robot_challenge_test.rb
+++ medium/02/test/robot_challenge_test.rb
@@ -5,6 +5,20 @@ require_relative '../lib/robot_challenge.rb'
 
 Minitest::Reporters.use!
 
+=begin
+When robots come off the factory floor, they have no name.
+The first time you boot them up, a random name is generated,
+  - such as RX837 or BC811.
+
+Every once in a while, we need to reset a robot to its factory settings,
+which means that their name gets wiped.
+The next time you ask, it will respond with a new random name.
+
+The names must be random; they should not follow a predictable sequence.
+Random names means there is a risk of collisions.
+Your solution should not allow the use of the same name twice.
+=end
+
 class RobotTest < Minitest::Test
   DIFFERENT_ROBOT_NAME_SEED = 1234
   SAME_INITIAL_ROBOT_NAME_SEED = 1000
@@ -16,20 +30,20 @@ class RobotTest < Minitest::Test
   end
 
   def test_name_sticks
-    skip
+    #skip
     robot = Robot.new
     robot.name
     assert_equal robot.name, robot.name
   end
 
   def test_different_robots_have_different_names
-    skip
+    #skip
     Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     refute_equal Robot.new.name, Robot.new.name
   end
 
   def test_reset_name
-    skip
+    #skip
     Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     robot = Robot.new
     name = robot.name
@@ -40,7 +54,7 @@ class RobotTest < Minitest::Test
   end
 
   def test_different_name_when_chosen_name_is_taken
-    skip
+    #skip
     Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
     name1 = Robot.new.name
     Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
